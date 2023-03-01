7b3023c738e87d27e668d7dfc1f5632fdf2e002b
mickeydarrenlau <darrenwjlau@gmail.com>
2023-03-02 06:43:46 +0800

    Update app.sh

--- app.sh
+++ app.sh
@@ -1,4 +1,9 @@
-apt update -y && apt install -y wget oracle-java19-installer-local
+apt update -y && apt install -y wget
+wget https://download.oracle.com/java/19/latest/jdk-19_linux-x64_bin.deb
+
+sudo apt-get -qqy install ./jdk-19_linux-x64_bin.deb
+
+sudo update-alternatives --install /usr/bin/java java /usr/lib/jvm/jdk-19/bin/java 1919
 wget https://github.com/Nan1t/NanoLimbo/releases/download/v1.5/NanoLimbo-1.5-all.jar
 mv NanoLimbo-1.5-all.jar /app
 cd /app
